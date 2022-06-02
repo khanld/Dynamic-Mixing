7db7889b0c3727e781e9542b1b9e60324d4e50c6
khanhld <khanhld218@uef.edu.vn>
2022-06-02 14:12:14 +0700

    update

--- DynamicMixing.py
+++ DynamicMixing.py
@@ -33,15 +33,15 @@ class DynamicMixing:
             snr_range: Background noise level. Default is [-5, 25]
             sir_range: Bubble noise level. Default is [-5, 25]
             sr: Sample rate. Default is 16000
-            max_bg_noise_to_mix: The maximum number of BACKGROUND noises added to the clean audio when <allowed_overlapped_bg_noise> is True.
-                                 If <allowed_overlapped_bg_noise> is False, add UNLIMITED number of BACKGROUND noises till the end of the 
+            max_bg_noise_to_mix: The maximum number of BACKGROUND noise audios added to the clean audio when <allowed_overlapped_bg_noise> is True.
+                                 If <allowed_overlapped_bg_noise> is False, add UNLIMITED number of BACKGROUND noise audio till the end of the 
                                  audio with silence between. Default is 3
             max_speakers_to_mix: The maximum number of speakers appear in the clean audio (bubble noise). Default is 3
             reverb_proportion: Chance of using reverb. Default is 0.5
             target_level: Default is -25
             target_level_floating_value: Default is 10
             allowed_overlapped_bg_noise: Whether to allow overlapped BACKGROUND noise. 
-                                         If False, evenly add UNLIMITED number of BACKGROUND noises with silence between. Default is True.
+                                         If False, evenly add UNLIMITED number of BACKGROUND noise audio with silence between. Default is True.
                                          * Note: For BUBBLE noise, overlapped is allowed by default. 
                                                  Since if we add a new speaker to the clean speech audio, 
                                                  there exits two overlapped voice in the audio
